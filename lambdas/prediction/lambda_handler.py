4d33667e4d62b3413bc3cbcadc3abff23ed9e822
Klimentina Dzeparoska <klimentinadjeparoska@gmail.com>
2021-10-12 23:32:52 +0200

    prediction service - added vocabulary

--- lambdas/prediction/lambda_handler.py
+++ lambdas/prediction/lambda_handler.py
@@ -9,13 +9,24 @@ ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
 runtime= boto3.client('runtime.sagemaker')
 
 
+def download_vocabulary ():
+    bucket = 'sagemaker-model-vocabularies' # replace with your bucket name
+    key = 'keras-model/vocabulary.json' # replace with your object key
+
+    s3 = boto3.client('s3')
+    response = s3.get_object(Bucket=bucket, Key=key)
+    content = response['Body']
+    file = json.load(content)
+    return file
+
+
 def pad_sequence(array, length, padding, value):
     num_zeros = length - len(array)
     new_array = []
     for x in range(0, num_zeros):
         new_array.append(value)
     for x in range(0, len(array)):
-        new_array.append(x)
+        new_array.append(array[x])
     return new_array
 
 
@@ -27,9 +38,8 @@ def word_tokenize(sentence):
     return sentence.split(' ')
 
 def format_data_for_prediction (text):
-    vocabulary = []
-    vocabulary_map = {}
-    counter = 1
+    vocabulary_map = download_vocabulary()
+    counter = len(vocabulary_map.keys()) + 1
 
     text_represented_with_numbers = []
     # break the text into sentences
@@ -47,15 +57,13 @@ def format_data_for_prediction (text):
             word = word.lower()
 
             if word != '' and word != 's':
-                if not vocabulary.__contains__(word):
-                    vocabulary.append(word)
+                if not vocabulary_map.keys().__contains__(word):
                     vocabulary_map[word] = counter
                     counter += 1
 
                 text_represented_with_numbers.append(vocabulary_map[word])
     # arrays to have same length
     padded_seq = pad_sequence(text_represented_with_numbers, length=63, padding='pre', value=0)
-    print(len(vocabulary))
     return padded_seq
 
 
@@ -64,6 +72,7 @@ def lambda_handler(event, context):
     print(data['body'])
 
     payload = format_data_for_prediction(data['body'])
+    print(payload)
     converted = json.dumps(payload)
 
     response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
